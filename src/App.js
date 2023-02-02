413cbb39a5dce5ad123d1da9a1097d066d575369
unknown <nguyenmai0102.meo@gmail.com>
2023-02-02 14:32:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from "axios";
+import React from "react";
+const routeAPI = "https://jsonplaceholder.typicode.com/posts"
 
-function App() {
+export default function App() {
+  const [post, setPost] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    //bao 404
+    axios
+    .get(`${routeAPI}/asdf`)
+    .then((response) =>{
+      setPost(response.data);
+    })
+    .catch((error) => {
+      setError(error);
+    });
+  }, []);
+
+  if(error) return `Error: ${error.message}`;
+  if(!post) return "No post";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
-  );
-}
+  )
 
-export default App;
+// React.useEffect(()=>{
+//   axios.get(routeAPI).then((response)=>{
+//     setPost(response.data)
+//   });
+// }, []);
+// React.useEffect(()=>{
+//   axios.get(`${routeAPI}/1`).then((response)=>{
+//     setPost(response.data)
+//   });
+// }, []);
+
+// function createPost(){
+//   axios
+//   .post(routeAPI,{
+//     title: "Hello World!",
+//     body:"This is a new post.",
+//   })
+//   .then((response)=>{
+//     setPost(response.data);
+//   });
+// }
+
+// function updatePost(){
+//   axios
+//   .put(`${routeAPI}1`,{
+//     title: "Hello World!",
+//     body: "this is an updated post",
+//   })
+//   .then((response)=>{
+//     setPost(response.data);
+//   })
+// }
+
+// function deletePost(){
+//   axios.delete(`${routeAPI}/1`).then(()=>{
+//     alert("Post deleted!");
+//     setPost(null);
+//   });
+// }
+
+
+
+// if(!post) return null;
+
+// return (
+//   <div>
+//     <h1>{post.title}</h1>
+//     <p>{post.body}</p>
+//   </div>
+// )
+
+// if(!post) return "No post!";
+
+// return(
+//   <div>
+//     <h1>{post.title}</h1>
+//     <p>post.body</p>
+//     <button onClick={createPost}>Create Post</button>
+//   </div>
+// );
+
+// return(
+//   <div>
+//     <h1>{post.title}</h1>
+//     <p>{post.body}</p>
+//     <botton onClick={updatePost}>Update Post</botton>
+//   </div>
+// )
+
+// return (
+//   <div>
+//     <h1>{post.title}</h1>
+//     <p>"{post.body}"</p>
+//      <button onClick={deletePost}> Delete Post</button>
+//   </div>
+// )
+ }
+
+
